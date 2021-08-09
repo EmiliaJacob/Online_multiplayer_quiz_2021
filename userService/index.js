54294f3456b1874e5efafb0b2fdacd4b34619355
Felix-Jacob <jacobfelix51@gmail.com>
2021-08-09 20:28:45 +0200

    cleaned code up

--- userService/index.js
+++ userService/index.js
@@ -51,15 +51,11 @@ app.post('/register',  (req,res) => {
 });
 	 
 function testing({data, headers, status}, userInfo, res) {
-    console.log("USERINFO" + JSON.stringify(userInfo));
-    console.log("mango result: " + JSON.stringify(data));
-    console.log("DOCS LENGHT: " + data.docs.length);
     if(data.docs.length == 0) { // Check if username already exists
         res.json({answer:"userName already exists"});
         return;
     }
     else {
-        console.log("kldfjsldkfjdflskj" + JSON.stringify(data.userName));
         addUser(userInfo.userName, userInfo.password);
         res.json({answer:"new username"});
         return;
@@ -77,7 +73,6 @@ async function addUser(userName, password) { // Create new user in DB
         userName: JSON.stringify(userName),
         password: JSON.stringify(password)
     }).then(({data, headers, status}) => {
-        console.log("input: " + JSON.stringify(userName)+" "+JSON.stringify(password));
         console.log("sucessfully created a new user: " + JSON.stringify(data));
        // console.log(JSON.stringify(data));
         // data is json response
@@ -87,33 +82,5 @@ async function addUser(userName, password) { // Create new user in DB
         //console.log("error: " + err);
         // either request error occured
         // ...or err.code=EDOCCONFLICT if document with the same id already exists
-    });
-}
-
-async function checkIfUsernameExists(userName) {
-    let mangoQuery = { // TODO: Create Index
-        selector: {
-            "userName": {
-                "$eq": data.userName
-            }
-        }
-    };
-
-    couch.mango("users", mangoQuery, {}).then(({data, headers, status}) => {
-        console.log("mango result: " + JSON.stringify(data));
-        if(data.docs.length != 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    // data is json response
-    // headers is an object with all response headers
-    // status is statusCode number
-    }, err => {
-    // either request error occured
-    // ...or err.code=EDOCMISSING if document is missing
-    // ...or err.code=EUNKNOWN if statusCode is unexpected
-        console.log("mango error: " + err);
     });
 }
