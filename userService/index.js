0694cc8b1927c9f4ac3835b9731ff8b6dff71e7d
Felix-Jacob <jacobfelix51@gmail.com>
2021-08-09 20:23:54 +0200

    You can add now users to the db

--- userService/index.js
+++ userService/index.js
@@ -15,31 +15,24 @@ const couch = new NodeCouchDb({ // See the default values at https://www.npmjs.c
 });
 
 app.post('/register',  (req,res) => {
-    data = req.body;
+    var userInfo = req.body;
 
-    if(data.userName && data.password) {
+    if(userInfo.userName && userInfo.password) { // Check if required fields are part of request
         let mangoQuery = { // TODO: Create Index
             selector: {
                 "userName": {
-                    "$eq": data.userName
+                    "$eq": userInfo.userName
                 }
             }
         };
 
-        couch.mango("users", mangoQuery, {}).then(({data, headers, status}) => {
-            console.log("mango result: " + JSON.stringify(data));
-            if(data.docs.length != 0) {
-                res.json({answer:"userName already exists"});
-                return;
-            }
-            else {
-                res.json({answer:"new username"});
-                return;
-            }
+        couch.mango("users", mangoQuery, {}).then(({data, headers, status}) => 
         // data is json response
         // headers is an object with all response headers
         // status is statusCode number
-        }, err => {
+       // }
+        testing({data, headers, status}, userInfo, res)
+        , err => {
         // either request error occured
         // ...or err.code=EDOCMISSING if document is missing
         // ...or err.code=EUNKNOWN if statusCode is unexpected
@@ -51,24 +44,40 @@ app.post('/register',  (req,res) => {
         return;
     }
         
-    console.log("recieved following data: " + JSON.stringify(data));
-    addUser();
+    console.log("recieved following data: " + JSON.stringify(userInfo));
     //res.json({answer:"recieved register request"});
     //couch.createDatabase("test_database").then(() => {console.log("database created")}, err => {console.log("error: " + err)});
     //check if user already exists
 });
-	
+	 
+function testing({data, headers, status}, userInfo, res) {
+    console.log("USERINFO" + JSON.stringify(userInfo));
+    console.log("mango result: " + JSON.stringify(data));
+    if(data.docs.length != 0) { // Check if username already exists
+        res.json({answer:"userName already exists"});
+        return;
+    }
+    else {
+        console.log("kldfjsldkfjdflskj" + JSON.stringify(data.userName));
+        addUser(userInfo.userName, userInfo.password);
+        res.json({answer:"new username"});
+        return;
+    }
+}
+
 app.listen(port, () => {
     console.log('listening on port: ' + port);
 })
 
-async function addUser() {
+async function addUser(userName, password) { // Create new user in DB
     let uuid = await couch.uniqid(); // yes, this could be done way easier 
-    //console.log(uuid);
-    couch.insert("test_database", {
+    couch.insert("users", {
         _id: uuid[0],
-        field: ["sample", "data", true]
+        userName: JSON.stringify(userName),
+        password: JSON.stringify(password)
     }).then(({data, headers, status}) => {
+        console.log("input: " + JSON.stringify(userName)+" "+JSON.stringify(password));
+        console.log("sucessfully created a new user: " + JSON.stringify(data));
        // console.log(JSON.stringify(data));
         // data is json response
         // headers is an object with all response headers
